d40cb3a7d13b4a8ee736e15779fb6ed271d6ff49
TomasContessi <55562600+TomasContessi@users.noreply.github.com>
2022-06-07 02:11:27 -0300

    avance- se definio la clase graficador

---
+++ src/graficador.hpp
@@ -0,0 +1,87 @@
+#ifndef GRAFICADOR_H
+#define GRAFICADOR_H
+
+#include "EasyBMP.h"
+
+#include "mapa.hpp"
+#include "ronda.hpp"
+#include "graficas.hpp"
+
+//---------------------------------------------------------------------------------------------------------------------
+//                                   CONTESSI TOMAS 99199 ALORITMOS Y PROGRAMACION II
+//---------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------BATALLA CAMPAL 2 V2.2-------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------
+//                                         DEFINICION DE LA CLASE GRAFICADOR
+//---------------------------------------------------------------------------------------------------------------------
+
+class graficador
+{
+private:
+    BMP tablero;
+    BMP pantalla;
+    graficas_t graficas;
+
+    ronda* rondita;
+    mapa* mapita;
+
+    std::string observador;
+    std::string rutaSalida;
+    std::string rutaGraficas;
+
+
+
+    //pre:
+    //pos: construye el tablero que se va a mostrar sin las cartas y lo guarda en tablero
+    void construirTablero();
+
+    //pre:
+    //pos: agrega las cartas al tablero y lo guarda en pantalla
+    void agregarCartas();
+
+    //pre:
+    //pos: agrega el fondo al tablero y lo guarda en pantalla
+    void agregarFondo();
+
+    //pre:
+    //pos: le agrega las cartas y el fondo al tablero y lo guarda en pantalla
+    void contruirPantalla();
+
+public:
+
+    //pre:
+    //pos: crea un objeto graficador
+    graficador(mapa*,ronda*);
+
+    //pre:
+    //pos: destruye el graficador
+    ~graficador();
+
+    //pre:
+    //pos: grafica la pantalla que ve el jugador especificado
+    void graficarPantalla(std::string);
+
+    //pre:
+    //pos: selecciona la ruta donde se van a buscar las graficas
+    void selecRutaGraficas(std::string);
+
+    //pre:
+    //pos: selecciona la ruta donde se van a guardar las pantallas
+    void selecRutaSalida(std::string);
+
+    //pre:
+    //pos: carga la estructura de graficas leyendolas de la ruta especifiada en rutaGraficas
+    void cargarGraficas();
+
+};
+
+graficador::graficador(mapa*,ronda*)
+{
+}
+
+graficador::~graficador()
+{
+}
+
+
+#endif /* GRAFICADOR_H */
--- src/pruebas.cpp
+++ src/pruebas.cpp
@@ -9,13 +9,33 @@
 #include "casilla.hpp"
 #include "mapa.hpp"
 
+#include "EasyBMP.h"
 
 using namespace std;
 
 
 int main(){
     mapa* mapita;
-    std::vector<unsigned int> dim ={3,3,1};
+    BMP tierra;
+    tierra.ReadFromFile("../graphics_20/land.bmp");
+
+    BMP tablero;
+    tablero.SetBitDepth( 24 );
+
+    std::vector<unsigned int> dim ={3,2,1};
+    tablero.SetSize(20*dim[0],20*dim[1]);
+
+    RangedPixelToPixelCopy( tierra, 0,tierra.TellWidth()-1,
+                         tierra.TellHeight()-1 , 0, 
+                         tablero, 0,0 );	
+    
+    RangedPixelToPixelCopy( tierra, 0,tierra.TellWidth()-1,
+                         tierra.TellHeight()-1 , 0, 
+                         tablero, 0,20 );
+
+    Rescale(tablero,'H',400);
+    tablero.WriteToFile("../tableros/tablero_tst.bmp");
+
 
     mapita= new mapa(costa,dim);
 
